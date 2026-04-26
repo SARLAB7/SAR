cc1e6913b194041cda78e14500c33be1a7bd0b9a
Visionstudio <76199245+VisionStudi0@users.noreply.github.com>
2026-04-26 13:57:13 -0500

    Update Firebase configuration and imports

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,21 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCTAiCQ7hi-ZmBt2MYWzciELL-lB_SsE3A",
-  authDomain: "iku-menu-interactivo.firebaseapp.com",
-  projectId: "iku-menu-interactivo",
-  storageBucket: "iku-menu-interactivo.firebasestorage.app",
-  messagingSenderId: "59457697555",
-  appId: "1:59457697555:web:4a7c5fd069ad41936d4ce6",
-  measurementId: "G-KZZ81XXY8V"
+  apiKey: "AIzaSyAUg9wKDz0kb_rAABLMFXM7LXS7WS18hwY",
+  authDomain: "sarp-40571.firebaseapp.com",
+  projectId: "sarp-40571",
+  storageBucket: "sarp-40571.firebasestorage.app",
+  messagingSenderId: "457540042625",
+  appId: "1:457540042625:web:68fd49fc2428595afd7280",
+  measurementId: "G-MHZQJED827"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+const analytics = getAnalytics(app);
